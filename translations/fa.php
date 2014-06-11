82e728b5f39f6bd47e8a0825d44a706e34a7e673
mypresta <myprestashop@yahoo.com>
2014-06-11 13:31:13 +0430

    translate to persian

---
+++ translations/fa.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{inamad}prestashop>inamad_1ff824f889bf7b35a745b8113575569c'] = 'ماژول ای - نماد';
+$_MODULE['<{inamad}prestashop>inamad_c125f463827742af1f4ee9288e3bdcac'] = 'نمایش اینماد در فروشگاه شما';
+$_MODULE['<{inamad}prestashop>inamad_6af91e35dff67a43ace060d1d57d5d1a'] = 'تنظیمات بروز گردید .';
+$_MODULE['<{inamad}prestashop>inamad_f4f70727dc34561dfde1a3c529b6205c'] = 'تنظیمات';
+$_MODULE['<{inamad}prestashop>inamad_6ead07fcca259f55c43b7bee86935283'] = 'کد آی فریم';
+$_MODULE['<{inamad}prestashop>inamad_1197b4f6bcee73835d5a7525e82a848d'] = 'متن تاییدیه نماد اعتماد';
+$_MODULE['<{inamad}prestashop>inamad_e883a1b362fd8e77d7aed7ea7928d634'] = 'میزان کوچک نمایی نماد';
+$_MODULE['<{inamad}prestashop>inamad_97746d82fbf2263e56150842eee56c8e'] = 'عرض نماد';
+$_MODULE['<{inamad}prestashop>inamad_8b6d9fc5536c569f2ff8ae2fc569f2a5'] = 'ارتفاع نماد';
+$_MODULE['<{inamad}prestashop>inamad_3fc68ea2d52f1413b99c6c2dea1615ad'] = 'مکان نمایش';
+$_MODULE['<{inamad}prestashop>inamad_4479f7c634534154bf3dbefb6d577b0f'] = 'بالای سایت - صفحه اصلی';
+$_MODULE['<{inamad}prestashop>inamad_213df96c71340431e0616fed74e4367a'] = 'وسط صفحه اصلی';
+$_MODULE['<{inamad}prestashop>inamad_e358e69ab1ad715e9506d52395a65c0c'] = 'ستون راست در صفحه اصلی';
+$_MODULE['<{inamad}prestashop>inamad_ad61457edf626798b38752b883e4d1c4'] = 'ستون چپ در صفحه اصلی';
+$_MODULE['<{inamad}prestashop>inamad_f3130713a5511659ae72b8a5cd75aea8'] = 'فوتر سایت';
+$_MODULE['<{inamad}prestashop>inamad_c9cc8cce247e49bae79f15173ce97354'] = 'ذخیره';
+$_MODULE['<{inamad}prestashop>inamad_630f6dc397fe74e52d5189e2c80f282b'] = 'بازگشت به لیست';
